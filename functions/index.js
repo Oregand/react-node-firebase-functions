6e1d29ad630dc04f735c826b3cbfda274a19edd9
David <davidoregan91@gmail.com>
2019-12-16 15:48:54 +0000

    feat(firebase): user auth functions

--- functions/index.js
+++ functions/index.js
@@ -1,55 +1,29 @@
 const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const app = require('express')();
+const FBAuth = require('./util/fbAuth');
 
+const cors = require('cors');
+app.use(cors());
 
-admin.initializeApp();
+const { db } = require('./util/admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from world!");
-});
+const {
+    getAllScreams,
+    postOneScream,
+    getScream,
+    commentOnScream,
+    likeScream,
+    unlikeScream,
+    deleteScream
+  } = require('./handlers/screams');
+  const {
+    signup,
+    login,
+    uploadImage,
+    addUserDetails,
+    getAuthenticatedUser,
+    getUserDetails,
+    markNotificationsRead
+  } = require('./handlers/users');
 
-app.get(`/screams`, (request, response) => {
-    admin
-    .firestore()
-    .collection('screams')
-    .orderBy('createdAt', `desc`)
-    .get()
-    .then((data) => {
-        let screams = []
-        data.forEach(doc => {
-            screams.push({
-                screamId: doc.id,
-                bdoy: doc.data().body,
-                userHandle: doc.data().userHandle,
-                createdAt: doc.data().createdAts
-            })
-        });
-        return response.json(screams);
-    })
-    .catch(err => console.error(err));
-});
-
-app.post(`/scream`, (request, response) => {
-    if(request.method !== 'POST') return response.status(400).json({ error: 'Method now allowed' });
-
-    const newScream = {
-        body: request.body,
-        user: request.body.userHandle,
-        createdAt: new Date().toISOString()
-    }
-
-    admin
-        .firestore()
-        .collection('screams')
-        .add(newScream)
-        .then(doc => {
-            response.json({ message: `${doc.id}: Created!` })
-        })
-        .catch(err => {
-            response.status(500).json({message: 'Something went wrong'  });
-            console.error(err);
-        });
-});
+  
