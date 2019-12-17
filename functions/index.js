5ad327f0ba2c2eb133093338f402a9fa8e967c38
David <davidoregan91@gmail.com>
2019-12-17 22:08:24 +0000

    feat(config): Update config

--- functions/index.js
+++ functions/index.js
@@ -3,15 +3,15 @@ const admin = require('firebase-admin');
 const express = require('express')();
 const firebase = require('firebase');
 const app = express();
-
 const config = require('./util/config');
+
+const db = admin.firestore();
 
 admin.initializeApp();
 firebase.initializeApp(config)
 
 app.get(`/screams`, (request, response) => {
-    admin
-    .firestore()
+    db
     .collection('screams')
     .orderBy('createdAt', `desc`)
     .get()
@@ -60,7 +60,32 @@ app.post('/signup', (req, res) => {
     handle: req.body.handle
   }
 
-  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  db
+  .doc(`/users/${newUser.handle}`)
+  .get()
+  .then(doc => {
+    if(doc.exists) {
+      return res.status(400).json({ message: 'User already exisits'  })
+    } else {
+      firebase
+      .auth()
+      .createUserWithEmailAndPassword(newUser.email, newUser.password)
+    }
+  })
+  .then(data => {
+    return data.user.getIdToken();
+  })
+  .then(token => {
+    return res.status(201).json({ token });
+  })
+  .catch(err => {
+    console.error(err);
+    return res.status(500).json({ message: 'Nope' })
+  })
+
+  firebase
+  .auth()
+  .createUserWithEmailAndPassword(newUser.email, newUser.password)
   .then(data => {
     return res.status(200).json({ message: `user ${data.user.uid} signed up` });
   })
