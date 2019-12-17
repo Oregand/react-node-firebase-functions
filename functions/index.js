0127a003fd596f0affb10620fe48d004456ec9db
David <davidoregan91@gmail.com>
2019-12-17 19:19:25 +0000

    feat(config): Update config

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,13 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const express = require('express');
-const app = express()
+const express = require('express')();
+const firebase = require('firebase');
+const app = express();
+
+const config = require('./util/config');
 
 admin.initializeApp();
+firebase.initializeApp(config)
 
 app.get(`/screams`, (request, response) => {
     admin
@@ -46,6 +50,24 @@ app.post(`/scream`, (request, response) => {
         });
 });
 
-// https://baseurl.com/api
+// Sign Up
+
+app.post('/signup', (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle
+  }
+
+  firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+  .then(data => {
+    return res.status(200).json({ message: `user ${data.user.uid} signed up` });
+  })
+  .catch(err => {
+    console.err(err);
+    return res.status(500).json({ error: err.code });
+  })
+})
 
 exports.api = functions.region('us-central1').https.onRequest(app)
--- functions/util/config.js
+++ functions/util/config.js
@@ -1,8 +1,10 @@
 module.exports = {
-    apiKey: 'AIzaSyDhp6ZCY-kJ64R1eMgGBxYWOnXDlVb8Ork ',
-    authDomain: 'social-ape-1717c.firebaseapp.com',
-    databaseURL: 'https://social-ape-1717c.firebaseio.com',
-    projectId: 'social-ape-1717c',
-    storageBucket: 'social-ape-1717c.appspot.com',
-    messagingSenderId: '354040522108'
+    apiKey: "AIzaSyDhp6ZCY-kJ64R1eMgGBxYWOnXDlVb8Ork",
+    authDomain: "social-ape-1717c.firebaseapp.com",
+    databaseURL: "https://social-ape-1717c.firebaseio.com",
+    projectId: "social-ape-1717c",
+    storageBucket: "social-ape-1717c.appspot.com",
+    messagingSenderId: "1036626661722",
+    appId: "1:1036626661722:web:8908157a435d150aed6363",
+    measurementId: "G-83ECMKC0EF"
   };
